ecc8afdc4c942f91e00a4d7c1eed8fb8fec38aa8
baggette <58569691+Baggette@users.noreply.github.com>
2022-09-14 15:24:40 -0500

    updated

--- index.js
+++ index.js
@@ -13,19 +13,20 @@ const client = new Client({
 });
 client.on('ready', () => {
     console.log('Free games offline is online')
-    client.user.setPresence({ activities: [{ name: 'Telling people that the free games bot is offline', type: 'PLAYING' }], status: 'active' });
+    client.user.setPresence({ activities: [{ name: 'Telling people that the free games bot is back online', type: 'PLAYING' }], status: 'active' });
 });
 client.on('messageCreate', (message) =>{
     if(message.content.toLocaleLowerCase().startsWith(p)){
         const embed = new MessageEmbed()
         .setColor('#2F3136')
-        .setTitle('The Free games bot is offline and your command was not ran')
+        .setTitle('The Free games bot is online but your command was not ran because we switched to slash commands `/`!')
         .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-        .setDescription("You maybe be wondering why the bot is offline and if it will ever come back up, below will be some Q&A")
+        .setDescription("You maybe be wondering why or how the bot came back online and why you must use slash commands")
         .setThumbnail("https://cdn.discordapp.com/avatars/698117737175580692/fa6bd8376bcd544b2a577d5c4074530a.webp?size=80")
         .addFields(
-            {name:'Will the bot ever come back online?', value:"Sadly at the moment GameFreak does not have the resources to host the bot"},
-            {name:'Why is the bot offline?',value:'Because of the sheer size of free games bot GameFreaks tiny server cannot handle it'},
+            {name:'Why did the bot come back online?', value:"The man the myth the legend <@513792641016659971> has brought it back and is hosting it."},
+            {name:'Why does the bot use slash commands?',value:'Because as of august 26th verified bots need to apply for message intents (eg: for the bot to "see" messages).'},
+            {name:"How do I use these slash commands?", value:"type / and a menu should come up, type /help and select the one from free games bot."}
         )
         .setTimestamp()
         .setFooter({text:"Bot made by Baggette#4777", iconURL:"https://cdn.discordapp.com/avatars/887756464020672523/5261d8f56ece38a54d1e88d3316310b6.webp?size=80"})
@@ -41,7 +42,7 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command)
 }
 client.on('messageCreate', (message) => { 
-    if (!message.content.startsWith(Prefix) || message.author.bot || message.channelId === "872185514885791796") return;
+    if (!message.content.startsWith(Prefix) || message.author.bot return;
     const args = message.content.slice(Prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
